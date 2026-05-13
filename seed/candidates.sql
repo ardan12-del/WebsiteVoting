7a01b3e3641c6e3a3255e14ddf4ff5211447b4b3
ardan12-del <admaulanaa1@gmail.com>
2026-05-13 11:05:58 +0700

    Add files via upload

---
+++ seed/candidates.sql
@@ -0,0 +1,11 @@
+insert into candidates (
+  number,
+  name,
+  vision,
+  mission,
+  image_url
+)
+values
+(1, 'Ahmad Rizki', 'OSIS Kreatif', 'Modernisasi sekolah', '/candidate1.jpg'),
+(2, 'Nanda Putra', 'Sekolah Aktif', 'Event modern', '/candidate2.jpg'),
+(3, 'Salsa Putri', 'Sekolah Maju', 'Prestasi siswa', '/candidate3.jpg');
